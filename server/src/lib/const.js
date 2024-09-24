a23ff62b275f9511c0816f1ee2e35296b9517a31
rohit-web-tech <rohitdogra0126@gmail.com>
2024-09-24 20:33:11 +0530

    fix a another typo

--- server/src/lib/const.js
+++ server/src/lib/const.js
@@ -1,10 +1,6 @@
-import "dotenv/config";
-
-console.log(process.env.DOMAIN)
-
 export const options = {
     httpOnly: true,
     secure: true,
     sameSite : 'None' ,
-    domain : '.netlify.app'
+    domain : process.env.DOMAIN
 }
